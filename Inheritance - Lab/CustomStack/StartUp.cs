863edcb2ddac71a1a4e356c95c0e24d827c8a19c
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-10-29 15:28:39 +0200

    Add files via upload

---
+++ Inheritance - Lab/CustomStack/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+            stack.Push("1");
+            stack.Push("2");
+            stack.Push("3");
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange(new System.Collections.Generic.Stack<string>());
+        }
+    }
+}
