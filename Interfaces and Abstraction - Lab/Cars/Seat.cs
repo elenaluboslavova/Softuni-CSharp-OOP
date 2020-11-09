75ffc893997f51a83bfd3c7e9731f325a1ab237c
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-11-09 14:01:25 +0200

    Add files via upload

---
+++ Interfaces and Abstraction - Lab/Cars/Seat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} Seat {Model}");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString().TrimEnd();
+        }
+
+        public string Start()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Engine start");
+            return sb.ToString().TrimEnd();
+        }
+
+        public string Stop()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Breaaak!");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Interfaces and Abstraction - Lab/Cars/Tesla.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar, ICar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} Tesla {Model} with {Battery} Batteries");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString().TrimEnd();
+        }
+
+        public string Start()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Engine start");
+            return sb.ToString().TrimEnd();
+        }
+
+        public string Stop()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Breaaak!");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
