0ca81a96648fe5c9d03d5546160fdb4e2ecdeefd
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-11-12 09:57:15 +0200

    Add files via upload

---
+++ Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private readonly double height;
+        private readonly double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return this.height * this.width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (this.width + this.height);
+        }
+        public override string Draw()
+        {
+            return base.Draw() + " " + this.GetType().Name;
+        }
+    }
+}
---
+++ Polymorphism - Lab/Shapes/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5.5);
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.Draw());
+
+            Console.WriteLine();
+            Rectangle rectangle = new Rectangle(2.5, 5.5);
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.Draw());
+        }
+    }
+}
