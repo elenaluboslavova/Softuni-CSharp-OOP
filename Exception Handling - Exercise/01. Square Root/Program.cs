5ee345de9721e62d3c71038502086ec73c7dd3b7
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-11-21 21:27:08 +0200

    Add files via upload

---
+++ Exception Handling - Exercise/01. Square Root/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01._Square_Root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Sqrt(n));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
---
+++ Exception Handling - Exercise/04. Fixing Vol2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04._Fixing_Vol2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num1, num2;
+                byte result;
+
+                num1 = 30;
+                num2 = 60;
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine($"{num1} x {num2} = {result}");
+                Console.ReadLine();
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow");
+            }
+        }
+    }
+}
