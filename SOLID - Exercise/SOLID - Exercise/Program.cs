0e63d35b43fc92ea7e11e81366903add699cc5ef
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-11-25 09:47:09 +0200

    Add files via upload

---
+++ SOLID - Exercise/SOLID - Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLID___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
