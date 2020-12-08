09db5e72479c319ae9a3b082d0917444cc6ba4c3
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-12-08 11:37:50 +0200

    Add files via upload

---
+++ Exams/Exam - 12 Apr 2020/01. Structure_Skeleton/CounterStrike/Models/Guns/Gun.cs
@@ -0,0 +1,68 @@
+﻿using CounterStrike.Models.Guns.Contracts;
+using CounterStrike.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CounterStrike.Models.Guns
+{
+    public abstract class Gun : IGun
+    {
+        private string name;
+        private int bulletsCount;
+
+        public Gun(string name, int bulletsCount)
+        {
+            this.Name = name;
+            this.BulletsCount = bulletsCount;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidGunName);
+                }
+                this.name = value;
+            }
+        }
+
+        public int BulletsCount
+        {
+            get
+            {
+                return this.bulletsCount;
+            }
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidGunBulletsCount);
+                }
+                this.bulletsCount = value;
+            }
+        }
+
+        abstract protected int FireRate { get; }
+
+        public int Fire()
+        {
+            if (BulletsCount - FireRate >= 0)
+            {
+                BulletsCount -= FireRate;
+                return FireRate;
+            }
+            else
+            {
+                BulletsCount = 0;
+                return 0;
+            }
+        }
+    }
+}
---
+++ Exams/Exam - 12 Apr 2020/01. Structure_Skeleton/CounterStrike/Models/Guns/Rifle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CounterStrike.Models.Guns
+{
+    public class Rifle : Gun
+    {
+        public Rifle(string name, int bulletsCount)
+            : base(name, bulletsCount)
+        {
+        }
+
+        protected override int FireRate
+        {
+            get
+            {
+                return 10;
+            }
+        }
+    }
+}
