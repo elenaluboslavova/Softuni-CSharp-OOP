eeaf5e532cb164236fd6318bef18c7b2aaf4a531
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-12-21 11:46:23 +0200

    Add files via upload

---
+++ Mocking and Test Driven Development - Exercise/Chainblock/Models/Chainblock.cs
@@ -0,0 +1,150 @@
+﻿using Chainblock.Contracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Chainblock.Models
+{
+    public class Chainblock : IChainblock
+    {
+        private readonly Dictionary<int, ITransaction> _transactions;
+
+        public Chainblock()
+        {
+            this._transactions = new Dictionary<int, ITransaction>();
+        }
+
+        public int Count => this._transactions.Count;
+
+        public void Add(ITransaction tx)
+        {
+            if (this._transactions.ContainsKey(tx.Id))
+            {
+                throw new InvalidOperationException();
+            }
+            this._transactions.Add(tx.Id, tx);
+        }
+
+        public void ChangeTransactionStatus(int id, TransactionStatus newStatus)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("Id cannot be less or equal to 0.");
+            }
+
+            if (!Enum.IsDefined(typeof(TransactionStatus), newStatus))
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (!_transactions.ContainsKey(id))
+            {
+                throw new ArgumentException();
+            }
+
+            ITransaction transaction = _transactions[id];
+
+            if (transaction.Status == newStatus)
+            {
+                throw new Exception();
+            }
+
+            transaction.Status = newStatus;
+        }
+
+        public bool Contains(ITransaction tx)
+        {
+            if (!_transactions.ContainsValue(tx))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Contains(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            if (!_transactions.ContainsKey(id))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public IEnumerable<ITransaction> GetAllInAmountRange(double lo, double hi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetAllOrderedByAmountDescendingThenById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetAllReceiversWithTransactionStatus(TransactionStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetAllSendersWithTransactionStatus(TransactionStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITransaction GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetByReceiverAndAmountRange(string receiver, double lo, double hi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetByReceiverOrderedByAmountThenById(string receiver)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetBySenderAndMinimumAmountDescending(string sender, double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetBySenderOrderedByAmountDescending(string sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetByTransactionStatus(TransactionStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetByTransactionStatusAndMaximumAmount(TransactionStatus status, double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<ITransaction> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTransactionById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
