47e5819039c090ba0d1f46e3e2a8c91f96dc8381
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-12-21 11:36:22 +0200

    Add files via upload

---
+++ Mocking and Test Driven Development - Lab/INStock/Contracts/IProductStock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace INStock.Contracts
+{
+    public interface IProductStock : IEnumerable<IProduct>
+    {
+        int Count { get; }
+
+        void Add(IProduct product);
+
+        bool Contains(IProduct product);
+
+        IProduct Find(int index);
+
+        IProduct FindByLabel(string label);
+
+        IEnumerable<IProduct> FindAllInPriceRange(decimal lowerEnd, decimal higherEnd);
+
+        IEnumerable<IProduct> FindAllByPrice(decimal price);
+
+        IProduct FindMostExpensiveProduct();
+
+        IEnumerable<IProduct> FindAllByQuantity(int quantity);
+
+        IEnumerable<IProduct> GetEnumerator<IProduct>();
+    }
+}
