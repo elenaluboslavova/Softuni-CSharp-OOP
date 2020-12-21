6e2271990c82a8b77ef7786c7c4bc5d9289eac2e
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-12-21 11:26:38 +0200

    Add files via upload

---
+++ Mocking and Test Driven Development - Lab/FakeAxeAndDummy.Tests/HeroTests.cs
@@ -0,0 +1,25 @@
+﻿using FakeAxeAndDummy.Contracts;
+using FakeAxeAndDummy.Tests.Fakes;
+using Moq;
+using NUnit.Framework;
+
+[TestFixture]
+public class HeroTests
+{
+    [Test]
+    public void GivenHero_ShouldReceiveExperience_WhenAttackedTargetDies()
+    {
+        
+        Mock<ITarget> fakeTarget = new Mock<ITarget>();
+
+        fakeTarget.Setup(f => f.GiveExperience()).Returns(20);
+        fakeTarget.Setup(f => f.IsDead()).Returns(true); 
+
+        Mock<IWeapon> fakeWeapon = new Mock<IWeapon>();
+        Hero hero = new Hero("Pesho", fakeWeapon.Object);
+
+        hero.Attack(fakeTarget.Object);
+
+        Assert.AreEqual(20, hero.Experience);
+    }
+}
