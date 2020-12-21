c5a4b2511bf51bbc19013beecc82c55e248ae016
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-12-21 11:25:44 +0200

    Add files via upload

---
+++ Mocking and Test Driven Development - Lab/FakeAxeAndDummy/Axe.cs
@@ -0,0 +1,29 @@
+﻿using FakeAxeAndDummy.Contracts;
+using System;
+
+// Axe durability drop with 5 
+public class Axe : IWeapon
+{
+    private int attackPoints;
+    private int durabilityPoints;
+
+    public Axe(int attack, int durability)
+    {
+        this.attackPoints = attack;
+        this.durabilityPoints = durability;
+    }
+
+    public int AttackPoints { get; set; }
+    public int DurabilityPoints { get; set; }
+
+    public void Attack(ITarget target)
+    {
+        if (this.durabilityPoints <= 0)
+        {
+            throw new InvalidOperationException("Axe is broken.");
+        }
+
+        target.TakeAttack(this.attackPoints);
+        this.durabilityPoints -= 1;
+    }
+}
